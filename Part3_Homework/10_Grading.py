c0930d005039a8e1cef451296a33ce5be251536f
MarsBalazs <marius.balazs05@e-uvt.ro>
2025-11-20 10:02:57 +0000

    hm

--- Part3_Homework/10_Grading.py
+++ Part3_Homework/10_Grading.py
@@ -7,11 +7,14 @@
 # 70-79: C
 # 60-69: D
 # Below 60: F
-grade =int(input("What is your grade?"))
-if grade>=90:
-    print('this number is smaller than 1000')
-if integer<100:
-    print('this number is smaller than 100')
-if integer<10:
-    print('this number is smaller than 10')
-print("Thank you :3")
+grade =int(input("Type your grade"))
+if grade>=90 and grade <=100 :
+    print('A')
+elif grade>=80 and grade<=89:
+    print('B')
+elif grade>=70 and grade<=79:
+    print('C')
+elif grade>=60 and grade<=69:
+    print('D')
+elif grade<=60:
+    print('F')
